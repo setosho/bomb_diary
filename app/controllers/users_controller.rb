0aa0d6710e1c765268c0d1afe932f78057357495
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2020-01-16 18:28:09 +0900

    ログイン,ログアウトシステム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def new
     @user = User.new
   end
@@ -6,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render 'new'
@@ -16,10 +18,39 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "ユーザー情報を変更しました"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to new_user_path, notice: "ユーザーを削除しました"
+  end
+
+  def favorite_picture
+    @user = User.find(params[:id])
+    @favorites = current_user.favorite_feeds.all
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:user_name, :email, :password, :profile,
+                                 :password_confirmation, :image, :image_cache)
+  end
+
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      redirect_to new_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :feeds
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users, only: [:new, :create, :show]
 end
