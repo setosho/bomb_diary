48073310f13f14d0e41cad095e3b0b72835c2cea
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2020-01-21 02:38:45 +0900

    投稿の順番をidの降順にした

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,7 +4,7 @@ class FeedsController < ApplicationController
   # GET /feeds
   # GET /feeds.json
   def index
-    @feeds = Feed.all
+    @feeds = Feed.all.order(id: "DESC")
   end
 
   # GET /feeds/1
