95c4734eb2eee5e3e88e2d933b148e95ce2219c6
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2020-02-09 17:24:25 +0900

    herokuの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :feeds
 
   resources :sessions, only: [:new, :create, :destroy]
+  root 'sessions#new'
 
   resources :users#, only: [:new, :create, :show, :edit, :destroy]
 
